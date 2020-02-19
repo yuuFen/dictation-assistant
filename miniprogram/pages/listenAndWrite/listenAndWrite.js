c6f5ac7548794dacd32edae4e72bd9f5a4d8928d
yuuFen <603236742@qq.com>
2020-02-19 19:57:53 +0800

    删删改改

--- miniprogram/pages/listenAndWrite/listenAndWrite.js
+++ miniprogram/pages/listenAndWrite/listenAndWrite.js
@@ -1,20 +1,18 @@
-const db = wx.cloud.database();
-const _ = db.command;
-let plugin = requirePlugin("WechatSI");
-let manager = plugin.getRecordRecognitionManager();
-const innerAudioContext = wx.createInnerAudioContext();
-let that;
-
+const db = wx.cloud.database()
+const _ = db.command
+let plugin = requirePlugin('WechatSI')
+let manager = plugin.getRecordRecognitionManager()
+const innerAudioContext = wx.createInnerAudioContext()
 
 Page({
   data: {
-    current: 0,//当前所在滑块的 index
-    scrollLeft: -90,//滚动条的位置,一个选项卡宽度是90（自定义来自css），按比例90*n设置位置
+    current: 0, //当前所在滑块的 index
+    scrollLeft: -90, //滚动条的位置,一个选项卡宽度是90（自定义来自css），按比例90*n设置位置
     conlist: [],
   },
 
   //tab切换
-  tab: function (event) {
+  tab: function(event) {
     // console.log(event.target.dataset.current);
     this.setData({ current: event.target.dataset.current })
     //锚点处理
@@ -23,7 +21,7 @@ Page({
     })
   },
   //滑动事件
-  eventchange: function (event) {
+  eventchange: function(event) {
     console.log(event.detail.current)
     this.setData({ current: event.detail.current })
     //锚点处理
@@ -33,15 +31,15 @@ Page({
   },
 
   // 转去 listenDetail
-  toDetail: function (e) {
-    let content = '';
-    let speak = '';
-    for (let word of e.currentTarget.dataset.content.content) {
-      content = content + word + '/';
+  toDetail: function(e) {
+    let content = ''
+    let speak = ''
+    for (const word of e.currentTarget.dataset.content.content) {
+      content = content + word + '/'
     }
     if (e.currentTarget.dataset.content.speak) {
-      for (let word of e.currentTarget.dataset.content.speak) {
-        speak = speak + word + '/';
+      for (const word of e.currentTarget.dataset.content.speak) {
+        speak = speak + word + '/'
       }
     }
     wx.navigateTo({
@@ -52,101 +50,101 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.showLoading({
       title: '加载中',
-    });
+    })
     console.log(options.book)
-    that = this;
+    let that = this
     // setNavigationBarTitle
-    let book = '';
-    let bookLevel = {
-      "11": "一年级上册",
-      "12": "一年级下册",
-      "21": "二年级上册",
-      "22": "二年级下册",
-      "31": "三年级上册",
-      "32": "三年级下册",
-      "41": "四年级上册",
-      "42": "四年级下册",
-      "51": "五年级上册",
-      "52": "五年级下册",
-      "61": "六年级上册",
-      "62": "六年级下册",
+    const bookLevel = {
+      '11': '一年级上册',
+      '12': '一年级下册',
+      '21': '二年级上册',
+      '22': '二年级下册',
+      '31': '三年级上册',
+      '32': '三年级下册',
+      '41': '四年级上册',
+      '42': '四年级下册',
+      '51': '五年级上册',
+      '52': '五年级下册',
+      '61': '六年级上册',
+      '62': '六年级下册',
     }
-    if (options.book.search("ch") != -1) { book += '语文' } else if (options.book.search("en") != -1) { book += '英语' } 
-    if (options.book.search("su") != -1) { book += '苏教版' } else if (options.book.search("zh") != -1) { book += '浙教版' } else if (options.book.search("rn") != -1 && (options.book.search("4") != -1 || options.book.search("5") != -1 || options.book.search("6") != -1)) { book += '人教版' } else { book += '部编版' } 
+    let book = ''
+    if (options.book.search('ch') != -1) {
+      book += '语文'
+    } else if (options.book.search('en') != -1) {
+      book += '英语'
+    }
+    if (options.book.search('su') != -1) {
+      book += '苏教版'
+    } else if (options.book.search('zh') != -1) {
+      book += '浙教版'
+    } else {
+      book += '部编版'
+    }
     for (let key in bookLevel) {
-      if (options.book.search(key) != -1) { 
-        book += bookLevel[key] 
+      if (options.book.search(key) != -1) {
+        book += bookLevel[key]
       }
     }
     wx.setNavigationBarTitle({
-      title: book
+      title: book,
     })
-    
-    let dbBook = 'write_' + options.book;
-    let conlist = [];
+
+    let dbBook = 'write_' + options.book
+    let conlist = []
     // 使用云函数，能读100条
-    wx.cloud.callFunction({
-      name: 'getContent',
-      data: {
-        dbBook: dbBook
-      }
-    }).then(res => {
-      that.setData({
-        conlist: res.result
-      });
-      wx.hideLoading();
-    })
+    wx.cloud
+      .callFunction({
+        name: 'getContent',
+        data: {
+          dbBook: dbBook,
+        },
+      })
+      .then((res) => {
+        that.setData({
+          conlist: res.result,
+        })
+        wx.hideLoading()
+      })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    innerAudioContext.offPlay();
+  onUnload: function() {
+    innerAudioContext.offPlay()
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function() {},
 })
